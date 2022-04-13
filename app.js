8f40eca63f70f8069779fedcd0ab6c56253a7812
Steven Kristjan <s.rekand@voco.ee>
2022-04-13 19:55:42 +0200

    Final commit, added videos

--- app.js
+++ app.js
@@ -1,6 +1,54 @@
-$(document).ready(function() {
-    $('#headerVideoLink').magnificPopup({
-      type:'inline',
-      midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-    });         
-  });
+function Toggle(){
+    var video1 = document.querySelector(".video1")
+    var audio = document.getElementById("audio1")
+
+    video1.classList.toggle("active")
+
+    audio.pause()
+    audio.currentTime = 0
+}
+function Toggle2(){
+    var video2 = document.querySelector(".video2")
+    var audio2 = document.getElementById("audio2")
+
+    video2.classList.toggle("active")
+
+    audio2.pause()
+    audio2.currentTime = 0
+}   
+function Toggle3(){
+    var video3 = document.querySelector(".video3")
+    var video = document.getElementById("audio3")
+
+    video3.classList.toggle("active")
+
+    audio.pause()
+    audio.currentTime = 0
+}
+function Toggle4(){
+    var video4 = document.querySelector(".video4")
+    var audio = document.getElementById("audio4")
+
+    video4.classList.toggle("active")
+
+    audio.pause()
+    audio.currentTime = 0
+}
+function Toggle5(){
+    var video5 = document.querySelector(".video5")
+    var audio = document.getElementById("audio5")
+
+    video5.classList.toggle("active")
+
+    audio.pause()
+    audio.currentTime = 0
+}   
+function Toggle6(){
+    var video6 = document.querySelector(".video6")
+    var audio = document.getElementById("audio6")
+
+    video6.classList.toggle("active")
+
+    audio.pause()
+    audio.currentTime = 0
+}   
